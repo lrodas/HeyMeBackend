02f7c6fe10e9b5276770100d1c6204783359505b
joselito9129@gmail.com <joselito9129@gmail.com>
2021-04-11 17:59:46 -0600

    configuracion url bienvenida

--- src/main/java/com/cycsystems/heymebackend/restcontrollers/UsuarioController.java
+++ src/main/java/com/cycsystems/heymebackend/restcontrollers/UsuarioController.java
@@ -517,8 +517,8 @@ public class UsuarioController {
 						+ new Date() + "\"}";
 				String hash = Base64.getEncoder().encodeToString(jsonData.getBytes());
 				try {
-					mailService.sendEmail(this.MAIL_FROM, response.getUsuario().getUsername(), this.SUBJECT_MAIL, hash,
-							MAIL_TEMPLATE_CONFIRM);
+					mailService.sendEmail(this.MAIL_FROM, response.getUsuario().getUsername(), this.SUBJECT_MAIL,
+							"https://heyme.com.gt/#/login?" + hash, MAIL_TEMPLATE_CONFIRM);
 				} catch (MessagingException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
